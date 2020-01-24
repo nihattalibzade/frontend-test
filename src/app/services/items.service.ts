64615dc8caac951bcc68091ece05014fff85e139
nihattalibzade <nihat.aze@gmail.com>
2020-01-24 03:53:46 +0100

    Implement Items service

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
 
-  constructor() { }
+  API_URL = 'http://localhost:3000/';
+  constructor(private http: HttpClient) { }
+
+  getAllItems() {
+    return this.http.get(`${this.API_URL}items`);
+  }
+
+  getItem(itemId) {
+    return this.http.get(`${this.API_URL + 'items'}/${itemId}`);
+  }
 }
