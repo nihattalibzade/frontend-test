3a109def50e38abe213794c037e701862a438766
nihattalibzade <nihat.aze@gmail.com>
2020-01-24 06:26:06 +0100

    Function to create tree view of items

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -14,10 +14,29 @@ export class BodyComponent implements OnInit {
   items: Item[] = [];
   constructor(private itemsService: ItemsService) {}
 
+  static createTreeView(flatItems) {
+    const map = {};
+    let node;
+    const roots = [];
+    for (let i = 0; i < flatItems.length; i += 1) {
+      map[flatItems[i].id] = i;
+      flatItems[i].children = [];
+    }
+    for (const item of flatItems) {
+      node = item;
+      if (node.parent_id) {
+        flatItems[map[node.parent_id]].children.push(node);
+      } else {
+        roots.push(node);
+      }
+    }
+    return roots;
+  }
   ngOnInit() {
     this.itemsService.getAllItems().subscribe((data: Item[]) => {
         this.items = data;
     });
+    this.itemsTreeView = BodyComponent.createTreeView(this.items);
   }
 
 }
