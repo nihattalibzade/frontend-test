96afdd4c1a2bb5c1e2386e5d756245b4585e532d
nihattalibzade <nihat.aze@gmail.com>
2020-01-24 03:23:14 +0100

    Create components, interface and service. Assets added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ItemsService } from './services/items.service';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, BodyComponent, SidebarComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
-  providers: [],
+  providers: [ItemsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
